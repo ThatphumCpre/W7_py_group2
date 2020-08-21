f0e8566d649e57551b25a7202dfa567f16441c4f
Thatphum <s6201012620112@email.kmutnb.ac.th>
2020-08-21 13:01:22 +0700

    -Fixed Error when input over upper,lower bound

--- Tictactor.py
+++ Tictactor.py
@@ -4,37 +4,43 @@ class Board() :
         self.player="o"
         self.round=1
         self.text = TextInput();
+
     def start(self) :
         while(True) :
-            self.text.placeMarker(self.player,self)
+            print("Player",self.player,"Turn ")
+            self.text.placeMarker(self.player, self)
             print(self.listBoard)
             if self.round%2==1 :
                 self.player = "o"
             else :
                 self.player = "x"
+
     def getBoard(self,position_x,position_y) :
         return self.listBoard[position_x][position_y]
+
     def setBoard(self,position_x, position_y) :
-        print(type(position_x))
         self.listBoard[position_x][position_y] = self.player
         self.round += 1
+
     def winCheck(self) :
         pass
+
     def clearBoard(self) :
         self.listBoard = [[" "," "," "],[" "," "," "],[" "," "," " ]]
+
 class TextInput() :
     def placeMarker(self, player,obj) :
         x = int(input("insert x : "))
         y = int(input("insert y : "))
-        if((x>2 or x<0 or y>2 or y<2) and (obj.getBoard(x,y)!=" ")) :
+        if((x>2 or x<0 or y>2 or y<2)) :
             print("Error Insert again")
-        elif(obj.getBoard(x,y)=="o" or obj.getBoard(x,y)=="x") :
-            print("Overlap Please try again")
+        elif (obj.getBoard(x,y)!=" ") :
+            print("Overlap Insert again")
         else :
             obj.setBoard(x, y)
 
 class Printer() :
-    def drawBoard(self) :
+    def drawBoard(self,obj) :
         pass
 
 game1=Board()
