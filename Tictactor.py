67301f8e3d3e85e9addc141940ba486ad992169d
Thatphum <s6201012620112@email.kmutnb.ac.th>
2020-08-21 12:40:10 +0700

    "Board Class and getter,setter"

--- Tictactor.py
+++ Tictactor.py
@@ -3,23 +3,39 @@ class Board() :
         self.listBoard = [[" "," "," "],[" "," "," "],[" "," "," " ]]
         self.player="o"
         self.round=1
-    def setBoard(self) :
-        pass
-    def getValues(self) :
-        pass
-    def setValues(self) :
-        pass
+        self.text = TextInput();
+    def start(self) :
+        while(True) :
+            self.text.placeMarker(self.player,self)
+            print(self.listBoard)
+            if self.round%2==1 :
+                self.player = "o"
+            else :
+                self.player = "x"
+    def getBoard(self,position_x,position_y) :
+        return self.listBoard[position_x][position_y]
+    def setBoard(self,position_x, position_y) :
+        print(type(position_x))
+        self.listBoard[position_x][position_y] = self.player
+        self.round += 1
     def winCheck(self) :
         pass
     def clearBoard(self) :
-        pass
+        self.listBoard = [[" "," "," "],[" "," "," "],[" "," "," " ]]
 class TextInput() :
-    def put(self) :
+    def placeMarker(self, player,obj) :
         x = int(input("insert x : "))
         y = int(input("insert y : "))
+        if((x>2 or x<0 or y>2 or y<2) and (obj.getBoard(x,y)!=" ")) :
+            print("Error Insert again")
+        elif(obj.getBoard(x,y)=="o" or obj.getBoard(x,y)=="x") :
+            print("Overlap Please try again")
+        else :
+            obj.setBoard(x, y)
 
 class Printer() :
     def drawBoard(self) :
         pass
-text = TextInput();
-text.put()
+
+game1=Board()
+game1.start()
